424ba21e280f17d0f52151f3b0bb8d4d7c3e396c
ria975 <nanadurowaa105@gmail.com>
2022-12-14 10:02:49 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,16 @@ void print_to_98(int n)
 			if (i != 98)
 				if (i != 98);
 				else if (i == 98)
-					printf("%d\n", j);
+					printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
 }
