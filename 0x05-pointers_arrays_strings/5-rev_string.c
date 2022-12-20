6feff549e898eab9d29466ca38870270ee1f721b
ria975 <nanadurowaa105@gmail.com>
2022-12-20 13:05:54 -0800

    pointers arrays strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,23 @@
  */
 void rev_string(char *s)
 {
-	char rv * s[0];
-	int c = 0;
-	int i;
+	char tmp;
+	int i, len, len1;
 
-	while (s[c] != '\0')
-		c++;
-	for (i = 0; i < c; i++)
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		c--;
-		rv = s[i];
-		s[i] = s[c];
-		s[c] = rv;
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
-
